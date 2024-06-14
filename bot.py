3abdc858e7db8ed5c4697c002e88b94ad4efca06
Naeem <58908219+n-aeee-m@users.noreply.github.com>
2024-06-15 03:01:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,12 +5,9 @@ import os
 from dotenv import load_dotenv
 from telegram.constants import ParseMode
 from telegram import Update
-from telegram.ext import (ApplicationBuilder, CallbackContext, CommandHandler, filters,
-                          ContextTypes, MessageHandler, Updater)
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
-from db import (add_feed_source, get_all_sources, get_sources,
-                is_already_present, remove_feed_source,
-                update_source_timestamp)
+from db import add_feed_source, get_all_sources, get_sources, is_already_present, remove_feed_source, update_source_timestamp
 from feed import format_feed_item, get_feed_info, read_feed
 from archive import capture
 
@@ -23,36 +20,29 @@ async def add_feed(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_chat.id
     source = context.args[0]
     if is_already_present(user, source):
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=source + ' already exists.')
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f'{source} already exists.')
     else:
         add_feed_source(user, source)
         feed_info = get_feed_info(source)
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=source + ' added.')
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=feed_info)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f'{source} added.')
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=feed_info)
 
 async def remove_feed(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_chat.id
     source = context.args[0]
     if is_already_present(user, source):
         remove_feed_source(user, source)
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=source + ' removed.')
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f'{source} removed.')
     else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=source + ' does not exist.')
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f'{source} does not exist.')
 
 async def list_feeds(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     userId = update.effective_chat.id
     sources = get_sources(userId)
-    if len(sources):
-        await context.bot.send_message(
-            chat_id=userId, text="\n".join(sources))
+    if sources:
+        await context.bot.send_message(chat_id=userId, text="\n".join(sources))
     else:
-        await context.bot.send_message(
-            chat_id=userId, text="No sources added yet")
+        await context.bot.send_message(chat_id=userId, text="No sources added yet")
 
 async def archive_link(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_chat.id
@@ -62,36 +52,30 @@ async def archive_link(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_chat.id
-    text_received = update.message.text
-    await context.bot.send_message(
-        chat_id=user, text='To add a feed use /add feedurl')
+    await context.bot.send_message(chat_id=user, text='To add a feed use /add feedurl')
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_chat.id
-    await context.bot.send_message(
-        chat_id=user, text='To add a feed use /add feedurl')
+    await context.bot.send_message(chat_id=user, text='To add a feed use /add feedurl')
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hello {update.effective_chat.first_name}')
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('An error occurred')
     logger.error(msg="Exception while handling an update:", exc_info=context.error)
+    if update and update.effective_message:
+        await update.effective_message.reply_text('An error occurred')
 
 async def fetch_feeds(context: ContextTypes.DEFAULT_TYPE):
     sources = get_all_sources()
-    filter_words = os.getenv('EXCLUDE_WORDS').splitlines()
+    filter_words = os.getenv('EXCLUDE_WORDS', '').splitlines()
     for source in sources:
         feeds = read_feed(source["url"], filter_words)
         logger.info(f"Found {len(feeds)} feeds from {source['url']}")
-        entry_index = 0
         last_post_updated_time = int(source["last_updated"])
         new_last_post_updated_time = last_post_updated_time
 
-        for entry in feeds:
-            entry_index += 1
-            if entry_index > 10:
-                break
+        for entry in feeds[:10]:
             if hasattr(entry, 'published_parsed'):
                 post_updated_time = int(time.strftime("%Y%m%d%H%M%S", entry.published_parsed))
             elif hasattr(entry, 'updated_parsed'):
@@ -106,8 +90,7 @@ async def fetch_feeds(context: ContextTypes.DEFAULT_TYPE):
                     text=format_feed_item(entry),
                     parse_mode=ParseMode.HTML
                 )
-                if os.getenv('ARCHIVE_POSTS') == 'true':
-                    # Add the link to archive.org
+                if os.getenv('ARCHIVE_POSTS', 'false') == 'true':
                     capture(entry.link)
 
                 if post_updated_time > new_last_post_updated_time:
@@ -133,9 +116,7 @@ def main():
     application.add_error_handler(error)
 
     job_queue = application.job_queue
-    job_queue.run_repeating(
-        fetch_feeds, interval=int(os.getenv('FEED_UPDATE_INTERVAL')), first=10
-    )
+    job_queue.run_repeating(fetch_feeds, interval=int(os.getenv('FEED_UPDATE_INTERVAL', '3600')), first=10)
     application.run_polling()
 
 if __name__ == '__main__':
